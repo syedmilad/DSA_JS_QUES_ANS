3599a6ce051131c7085fd0b3d73a90d6564e0c18
syed milad <syedmilad99@gmail.com>
2024-02-22 11:50:03 +0500

    bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,39 @@
+// Bubble sorting in array
+// the soorting alogoritm where the largest values bubble up at the top;
+
+// const bubbleSort = (array) => {
+//   for (let i = array.length; i > 0; i--) { // length array kia length 3
+//     for (let j = 0; j < i (2) - 1; j++) { // #1) 3 - 1 = 2
+//       if (array[j] > array[j + 1]) {
+//         [array[j], array[j + 1]] = [array[j + 1], array[j]];
+//       }
+//     }
+//   }
+
+//   return array;
+// };
+
+// const result = bubbleSort([4, 3, 2]);
+// console.log(result);
+
+// const bubbleSort = (array) => {
+//   for (let i = array.length; i > 0; i--) { length=3 lenght = 2 lenght = 1
+//     for (let j = 0; j < i - 1; j++) {
+//       if (array[j] > array[j + 1]) {
+//         [array[j], array[j + 1]] = [array[j + 1], array[j]];
+//       }
+//     }
+//   }
+
+//   return array;
+// };
+
+// const result = bubbleSort([4, 3, 2]);
+// [4,3,2]
+// [3,4,2]
+// [3,2,4]
+
+// [3,2,4]
+// [2,3,4]
+// [2,3,4]
+// console.log(result);
